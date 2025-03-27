4cf4c20d04179e516ab1854d7bd65067a083f586
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2025-03-27 21:14:21 +0300

    Initial Commit

---
+++ FormatDateExplorer/FormatDateExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatDateExplorerApp.swift
+//  FormatDateExplorer
+//
+//  Created by Kennedy Karimi on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormatDateExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
